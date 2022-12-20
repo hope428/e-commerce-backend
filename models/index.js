986e676131c631b51519bacfab223789abcebeac
Leon Forsythe <lvforsythe95@gmail.com>
2022-12-20 16:36:45 -0500

    fixed through table include statement

--- models/index.js
+++ models/index.js
@@ -16,11 +16,13 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag, 
+  as: 'product_tags'
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag
+  through: ProductTag,
+  as: 'products'
 })
 
 module.exports = {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -9,7 +9,7 @@ router.get("/", async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tags = await Tag.findAll({
-      include: {model: Product}
+      include: { model: Product, through: ProductTag, as: 'products' }
     });
     res.status(200).json(tags);
   } catch (error) {
@@ -22,7 +22,7 @@ router.get("/:id", async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tag = await Tag.findByPk(req.params.id, {
-      include: { model: Product },
+      include: { model: Product, through: ProductTag, as: 'products' },
     });
 
     if (!tag) {
