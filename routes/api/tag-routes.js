43293de0d5625ff1d1723e37d724d796e3e39759
Leon Forsythe <lvforsythe95@gmail.com>
2022-12-19 20:18:15 -0500

    created tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,28 +1,72 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tags = await Tag.findAll();
+    res.status(200).json(tags);
+  } catch (error) {
+    res.status(404).json(error);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tag = await Tag.findByPk(req.params.id, {
+      include: { model: Product },
+    });
+
+    if (!tag) {
+      res.status(404).json("That tag id does not exist!");
+      return;
+    }
+    res.status(200).json(tag);
+  } catch (error) {
+    res.status(404).json(error);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post("/", async(req, res) => {
   // create a new tag
+  try {
+    if(!req.body.tag_name){
+      res.status(400).json("You must specify a tag name!")
+      return
+    }
+    const newTag = await Tag.create(req.body)
+    res.status(201).json(newTag)
+  } catch (error) {
+    res.status(404).json(error)
+  }
+  
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    if(!req.params.id){
+      res.status(404).json("You must supply an id")
+    }
+    const deletedTag = Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.status(200).json("Tag deleted")
+
+  } catch (error) {
+    res.status(404).json(error)
+  }
 });
 
 module.exports = router;
